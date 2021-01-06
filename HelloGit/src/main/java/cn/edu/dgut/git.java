db0f92313e18463772841dcc3b38e11d379cd78a
imap <774919369@qq.com>
2021-01-06 16:57:24 +0800

    添加文件

---
+++ HelloGit/src/main/java/cn/edu/dgut/git.java
@@ -0,0 +1,11 @@
+package cn.edu.dgut;
+
+/**
+ * @author HHH
+ * @create 2021-01-06 16:49
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("HELLO GIT!");
+    }
+}
